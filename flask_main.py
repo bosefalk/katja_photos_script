5f7f2621c7abb617a8aea588e87c7aae037014e7
Unknown <bosefalk@gmail.com>
2017-05-25 15:33:35 +0200

    delete cache

--- flask_main.py
+++ flask_main.py
@@ -8,3 +8,7 @@ app.config.from_object('config')
 @app.route('/brains/test1', methods=['GET', 'POST'])
 def test1():
 	return send_file('photos/test1/katerina_falk_photos.zip', as_attachment=True)
+
+@app.route('/brains/fuckup', methods=['GET', 'POST'])
+def fuckup():
+	return send_file('photos/fuckup/katerina_falk_photos.zip', as_attachment=True)
